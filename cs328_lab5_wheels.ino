28ef0fc92a916cf918e71d326d44bcf2302ccdfe
Raner <r4ner2000@gmail.com>
2025-11-03 11:02:14 -0500

    bluetoothStart

--- cs328_lab5_wheels.ino
+++ cs328_lab5_wheels.ino
@@ -1,6 +1,5 @@
 // Pins for all inputs, keep in mind the PWM defines must be on PWM pins
 // MOTOR WHEELS
-#include <SoftwareSerial.h>
 #define MotorPWM_A 4 //left motor
 #define MotorPWM_B 5 //right motor
 
@@ -15,10 +14,14 @@
 
 
 static volatile int16_t count_left=0;
+static volatile int16_t count_right = 0;
 
 // 3.215 = (60sec/0.1sec)/(48gear ratio * 4pulses/rev)
 float rotation=3.125;  
-float RPM=0;
+float RPM_left = 0;
+float RPM_right = 0;
+
+bool testStarted = false; // Keeps track if the test is running
 /***************************************/
 // This is the Interrupt Service Routine
 // for the left motor.  
@@ -26,6 +29,10 @@ float RPM=0;
 void ISRMotorLeft()
 {
   count_left++;
+}
+void ISRMotorRight() 
+{
+  count_right++;
 }
 
 void setup() {
@@ -39,16 +46,27 @@ void setup() {
     pinMode(INA2B, OUTPUT);
 
     pinMode(ENCODER_LEFT, INPUT_PULLUP);
+    pinMode(ENCODER_RIGHT, INPUT_PULLUP);
     attachInterrupt(digitalPinToInterrupt(ENCODER_LEFT), ISRMotorLeft, FALLING);
+    attachInterrupt(digitalPinToInterrupt(ENCODER_RIGHT), ISRMotorRight, FALLING);
 
     Serial.println("Starting test: \n");
 }
 
 void loop(){
-  delay(5000);
+ /* delay(5000);
   increaseSpeed();
   while(1);
+*/
+ if (Serial2.available()) {
+    String command = Serial2.readStringUntil('\n');
+    command.trim(); // remove newline or spaces
 
+    if (command.equalsIgnoreCase("start") && !testStarted) {
+      testStarted = true;
+      bluetoothStart();
+    }
+  }
 }
 // Method: Forward
 // Input: speed – value [0-255]
@@ -72,27 +90,36 @@ void Forward(int speed)
 void increaseSpeed() {
   for (int speed = 0; speed <= 255; speed += 5) {
     count_left=0;
+    count_right = 0;
+
     Forward(speed);
-
     delay(500); 
-    RPM = count_left * rotation;
+    RPM_left = count_left * rotation;
+    RPM_right = count_right * rotation;
 
-    sendBluetooth(RPM, speed);
+   sendBluetooth(speed, RPM_left, RPM_right);
   }
     analogWrite(MotorPWM_A, 0);
     analogWrite(MotorPWM_B, 0);
     Serial2.println("Test complete.");
 }
 
-void sendBluetooth(float RPM, int PWM){
+void sendBluetooth(float PWM,float leftRPM, float rightRPM ){
   Serial2.print(PWM);
   Serial2.print(",");
-  Serial2.println(RPM);
+  Serial2.print(leftRPM);
+  Serial2.print(",");
+  Serial2.println(rightRPM);
 }
 
-
-
-
+void bluetoothStart() {
+  Serial2.println("Starting motor test...");
+  Serial.println("Starting motor test...");
+  increaseSpeed();
+  Serial2.println("Test complete.");
+  Serial.println("Test complete.");
+  testStarted = false; // Allow restarting if needed
+}
 
 
 
